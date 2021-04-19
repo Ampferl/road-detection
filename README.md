841908e77d504c22417151aed94ebebb7e013e41
Jonas Ampferl <jonas@ampferl.com>
2021-04-19 19:33:44 +0200

    Documentation: Added Project Resources to the Readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Simple Road Line Detection
 ![](https://i.imgur.com/qT8PAAv.png)
 
+## Resources
+- [Python OpenCV](https://pypi.org/project/opencv-python/)
+- [Self-Driving-Car Python Tutorial](https://github.com/rslim087a/Self-Driving-Car-Course-Codes)
--- lanes.py
+++ lanes.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import sys
 
+global_left_fit_average = []
+global_right_fit_average = []
 
 def make_coords(image, line_parameters):
     slope, intercept = line_parameters
@@ -15,17 +17,27 @@ def make_coords(image, line_parameters):
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
-    for line in lines:
-        x1,y1,x2,y2 = line.reshape(4)
-        parameters = np.polyfit((x1,x2), (y1,y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        if slope < 0:
-            left_fit.append((slope, intercept))
-        else:
-            right_fit.append((slope, intercept))
-    left_fit_average = np.average(left_fit, axis=0)
+    global global_left_fit_average
+    global global_right_fit_average
+
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line.reshape(4)
+            parameters = np.polyfit((x1,x2), (y1,y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    if len(left_fit) == 0:
+        left_fit_average = global_left_fit_average
+    else:
+        left_fit_average = np.average(left_fit, axis=0)
+        global_left_fit_average = left_fit_average
+
     right_fit_average = np.average(right_fit, axis=0)
+    global_right_fit_average = right_fit_average
     left_line = make_coords(image, left_fit_average)
     right_line = make_coords(image, right_fit_average)
     return np.array([left_line, right_line])
@@ -50,7 +62,7 @@ def display_lane(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1,y1,x2,y2 in lines:
-            cv2.line(line_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 10)
+            cv2.line(line_image, (x1.astype(np.int64), y1.astype(np.int64)), (x2.astype(np.int64), y2.astype(np.int64)), (255, 0, 0), 10)
     return line_image
 
 
